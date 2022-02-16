f943519a6bbbb4660e52b9970f7998c2eecdd65c
artem.zinchenko <artem.zinchenko100@gmail.com>
2022-02-16 16:53:48 +0000

    add repository functional

--- src/main/java/ua/com/zinchenko/persistence/TestRepository.java
+++ src/main/java/ua/com/zinchenko/persistence/TestRepository.java
@@ -1,4 +1,15 @@
 package ua.com.zinchenko.persistence;
 
+import ua.com.zinchenko.service.model.Test;
+
+import java.util.List;
+
 public interface TestRepository {
+
+    Test[] getAllTests();
+    Test[] getTestsBySubject(String subject);
+    Test[] getTestsByForm(String form);
+    List<String> getAllSubjects();
+    List<String> getAllForms();
+
 }
--- src/main/java/ua/com/zinchenko/persistence/TestRepositoryImpl.java
+++ src/main/java/ua/com/zinchenko/persistence/TestRepositoryImpl.java
@@ -2,8 +2,10 @@ package ua.com.zinchenko.persistence;
 
 import ua.com.zinchenko.service.model.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TestRepositoryImpl implements TestRepository {
 
@@ -12,6 +14,39 @@ public class TestRepositoryImpl implements TestRepository {
     public TestRepositoryImpl() {
         init();
     }
+
+
+    @Override
+    public Test[] getAllTests() {
+        return tests;
+    }
+
+    @Override
+    public Test[] getTestsBySubject(String subject) {
+        return Arrays.stream(tests)
+                .filter(t -> t.getSubject().equalsIgnoreCase(subject))
+                .toArray(Test[]::new);
+    }
+
+    @Override
+    public Test[] getTestsByForm(String form) {
+        return Arrays.stream(tests)
+                .filter(t -> t.getForm().equalsIgnoreCase(form))
+                .toArray(Test[]::new);
+    }
+
+    @Override
+    public List<String> getAllSubjects() {
+        return Arrays.stream(tests)
+                .map(Test::getSubject)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> getAllForms() {
+        return Arrays.stream(tests)
+                .map(Test::getForm)
+                .collect(Collectors.toList());    }
 
     private void init() {
         tests[0] = new Test(
